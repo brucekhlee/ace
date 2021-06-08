c9670b4ff8cd1a8d570e29c82c5543420b5d082b
Kuo Hao Lee <bruce.oceans@gmail.com>
2021-06-08 16:38:16 -0400

    clean up

--- maestro/file_parser.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-file = 'Modafinal_Analogue_0212.txt'
-labels = ['Name', 'Smile']
-df = pd.read_csv(file, names=labels, sep='\t')
-for i in range(len(df)):
-    with open(df["Name"][i]+'.smi', "w") as f:
-        f.write(df["Smile"][i])
